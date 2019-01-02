ec68b55de1752bd408e2646878696a344774b274
Job Getabu <getabujob@gmail.com>
2019-01-02 14:42:25 +0300

    added resources

---
+++ eSchoolTests/SettingsUITests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using eSchool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSchool.Tests
+{
+    [TestClass()]
+    public  class SettingsUITests
+    {
+        [TestMethod()]
+        public void IsIdDuplicateuplicateTest()
+        {
+            //SettingsUI frm = new SettingsUI();
+            //Assert.AreEqual(await frm.IsIdDuplicateuplicate(1262),await frm.IsIdDuplicateuplicate(1262));
+            
+        }
+    }
+}
